597f8381d8084ac894a3f276af45ce09e23d95f3
KD24Ortega <kdortega@espe.edu.ec>
2025-10-31 17:25:07 -0500

    Agregar archivos de proyecto.

---
+++ WebApplication02_Con Autenticacion/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication02_Con_Autenticacion.MvcApplication" Language="C#" %>
---
+++ WebApplication02_Con Autenticacion/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication02_Con_Autenticacion.Startup))]
+namespace WebApplication02_Con_Autenticacion
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
